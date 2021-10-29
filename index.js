c830645b4ab775a9cb9f69376f3cc096d1eea83c
Leojoy62 <leojoy62@gmail.com>
2021-10-30 03:20:52 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,43 @@
+const express = require('express');
+const { MongoClient } = require('mongodb');
+require('dotenv').config();
+const cors = require('cors');
+
+const app = express();
+const port = process.env.PORT || 5000;
+
+//Middleware
+app.use(cors());
+app.use(express.json());
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.bgl6y.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+
+async function run() {
+    try {
+        await client.connect();
+        const database = client.db('travellj');
+        const productCollection = database.collection('packages');
+
+        //Get Products API
+        app.get('/products', async (req, res) => {
+            const cursor = productCollection.find({});
+            const products = await cursor.toArray();
+            res.send(products);
+        })
+    }
+    finally {
+        // await client.close();
+    }
+}
+
+run().catch(console.dir);
+
+
+app.get('/', (req, res) => {
+    res.send('Server is running');
+});
+
+app.listen(port, () => {
+    console.log('server running at port', port);
+});
